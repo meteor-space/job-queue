9817c9456f479f94b88d8f2e5867f06660993499
Rhys Bartels-Waller <rhyslbw@gmail.com>
2015-11-17 09:40:16 +1100

    Convert to template strings in logger

--- source/server/logger.js
+++ source/server/logger.js
@@ -7,49 +7,49 @@ Space.Object.extend(Space.jobQueue, 'Logger', {
   eventSubscriptions() {
     return [{
       'Space.jobQueue.JobServerStarted'() {
-        this.log.info("Job Server Started");
+        this.log.info(`Job Server Started`);
       },
       'Space.jobQueue.JobServerShutdown'() {
-        this.log.info("Job Server shutdown");
+        this.log.info(`Job Server shutdown`);
       },
       'Space.jobQueue.JobTakenByWorker'() {
-        this.log.info("Job taken by worker");
+        this.log.info(`Job taken by worker`);
       },
       'Space.jobQueue.JobRemoved'() {
-        this.log.info("Job removed");
+        this.log.info(`Job removed`);
       },
       'Space.jobQueue.JobPaused'() {
-        this.log.info("Job paused");
+        this.log.info(`Job paused`);
       },
       'Space.jobQueue.JobResumed'() {
-        this.log.info("Job resumed");
+        this.log.info(`Job resumed`);
       },
       'Space.jobQueue.JobReady'() {
-        this.log.info("Job ready");
+        this.log.info(`Job ready`);
       },
       'Space.jobQueue.JobCancelled'() {
-        this.log.warn("Job cancelled");
+        this.log.warn(`Job cancelled`);
       },
       'Space.jobQueue.JobRestarted'() {
-        this.log.info("Job restarted");
+        this.log.info(`Job restarted`);
       },
       'Space.jobQueue.JobAdded'(event) {
-        this.log.info(event.type + " job added");
+        this.log.info(`${event.type} job added`);
       },
       'Space.jobQueue.JobRerun'() {
-        this.log.info("Job rerun");
+        this.log.info(`Job rerun`);
       },
       'Space.jobQueue.JobProgressed'() {
-        this.log.info("Job progressed");
+        this.log.info(`Job progressed`);
       },
       'Space.jobQueue.JobLogAdded'() {
-        this.log.info("Job log added");
+        this.log.info(`Job log added`);
       },
       'Space.jobQueue.JobCompleted'() {
-        this.log.info("Job completed");
+        this.log.info(`Job completed`);
       },
       'Space.jobQueue.JobFailed'() {
-        this.log.warn("Job failed");
+        this.log.warn(`Job failed`);
       }
     }]
   }
