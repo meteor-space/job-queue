6f0032f87c5a26593bb0504e6af1f0a248ad466b
Rhys Bartels-Waller <rhyslbw@gmail.com>
2016-01-08 12:40:21 +1100

    Minor formatting

--- source/server/event-publisher.js
+++ source/server/event-publisher.js
@@ -15,78 +15,78 @@ Space.Object.extend(Space.jobQueue, 'EventPublisher', {
         case 'startJobServer':
           self.publish(new Space.jobQueue.JobServerStarted({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'shutdownJobServer':
           self.publish(new Space.jobQueue.JobServerShutdown({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'getWork':
           self.publish(new Space.jobQueue.JobTakenByWorker({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobRemove':
           this.publish(new Space.jobQueue.JobRemoved({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobPause':
           self.publish(new Space.jobQueue.JobPaused({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobResume':
           self.publish(new Space.jobQueue.JobResumed({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobReady':
           self.publish(new Space.jobQueue.JobReady({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobCancel':
           self.publish(new Space.jobQueue.JobCancelled({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobRestart':
           self.publish(new Space.jobQueue.JobRestarted({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobSave':
           self.publish(new Space.jobQueue.JobAdded({
             collection: self.jobCollection._name,
             type: message.params[0].type
-          }))
+          }));
           break;
         case 'jobRerun':
           self.publish(new Space.jobQueue.JobRerun({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobProgress':
           self.publish(new Space.jobQueue.JobProgressed({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobLog':
           self.publish(new Space.jobQueue.JobLogAdded({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobDone':
           self.publish(new Space.jobQueue.JobCompleted({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
         case 'jobFail':
           self.publish(new Space.jobQueue.JobFailed({
             collection: self.jobCollection._name
-          }))
+          }));
           break;
       }
     });
