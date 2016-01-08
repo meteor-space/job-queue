3ab3bb981c34e98cc406f0ab8dc77ea0564c8efc
Rhys Bartels-Waller <rhyslbw@gmail.com>
2016-01-08 21:25:59 +1100

    Change to dynamic publications based on config

--- source/server/publications.js
+++ source/server/publications.js
@@ -1,24 +1,36 @@
 Space.messaging.Publication.extend(Space.jobQueue, 'Publications', {
 
   dependencies: {
-    queue: 'Space.jobQueue.Jobs'
+    queue: 'Space.jobQueue.Jobs',
+    configuration: 'configuration'
+  },
+
+  _publications: null,
+
+  onDependenciesReady() {
+    this._publications = [];
+    let config = this.configuration.jobQueue;
+    if(config.remoteAccess.publish) {
+      this._publications.push({
+        'space-jobQueue-ready-jobs': (context, options = {}) => {
+          check(options, Object);
+          // Logged in users only -> later will be capability-based
+          if(context.userId === undefined) {
+            throw new Meteor.Error('Unauthorised Access');
+          }
+          let query = {status: 'ready'};
+          if(options.type !== undefined) {
+            query.type = options.type
+          }
+          return this.queue.find(query);
+        }
+      });
+    }
+    Space.messaging.Publication.prototype.onDependenciesReady.call(this);
   },
 
   publications() {
-    return [{
-      'space-jobQueue-ready-jobs': (context, options = {}) => {
-        check(options, Object);
-        // Logged in users only -> later will be capability-based
-        if(context.userId === undefined) {
-          throw new Meteor.Error('Unauthorised Access');
-        }
-        let query = { status: 'ready' };
-        if(options.type !== undefined) {
-          query.type = options.type
-        }
-        return this.queue.find(query);
-      }
-    }];
+    return this._publications;
   }
 
 });
